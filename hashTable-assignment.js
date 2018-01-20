ebb8bebc2e648668749b790c765486f73ef6ab33
Sultan Aslam <sultanaslam93@gmail.com>
2018-01-20 17:25:01 +0500

    retrieve & remove

--- hashTable-assignment.js
+++ hashTable-assignment.js
@@ -1,16 +1,3 @@
-// obj = {name: 'yasser'}
-// obj.name = 'yasser'
-// obj.name = 'mahmud'
-// obj.age = 45
-// use  agenerator 0 - 7, this will give an index
-// arr[1] arr[1] arr[2]
-// arr = [empty, [ ['name', 'yasser'], ['age', 45] ],..........empty]
-// arr = [location=empty, location[],]
-
-
-// This is a "hashing function". You don't need to worry about it, just use it
-// to turn any string into an integer that is well-distributed between the
-// numbers 0 and `max`
 var getIndexForKey = function(str, max) {
   var hash = 0;
   for (var i = 0; i < str.length; i++) {
@@ -21,8 +8,6 @@ var getIndexForKey = function(str, max) {
   return hash % max;
 };
 
-///////////////////////////////////////////////////////////////
-
 var HashTable = function() {
   this.limit = 8;
   this.storage = [];
@@ -31,26 +16,42 @@ var HashTable = function() {
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexForKey(k, this.limit);
 
-  if(!this.storage[index]) { // the location is empty [empty, ......]
-    this.storage[index] = []; //[[]. .....]
-    this.storage[index].push([k,v]) //[[[k,v]],.....]
-
-  } else { // location is not empty, means collision
-    for(var i = 0; i < this.storage[index].length; i++) { // iterate over the array in the selected location
-      if(k === this.storage[index][i][0]){ // if the key is already there
-        this.storage[index][i][1] = v; // replace the value with new value
+  if(!this.storage[index]) {
+    this.storage[index] = []
+    this.storage[index].push([k,v])
+  }else {
+    for(var i = 0; i < this.storage[index].length; i++) {
+      if(k === this.storage[index][i][0]) {
+        this.storage[index][i][1] =
         return; 
       }
     }
-    this.storage[index].push([k,v]); // if collisin but unique key then add it to the location array
+    this.storage[index].push([k,v]);
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
- // add your code here
+  var index = getIndexForKey(k, this.limit);
+
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (k === this.storage[index][i][0]) {
+      return this.storage[index][i][1];
+    }
+  }
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
-  // add your code here
-
+  var index = getIndexForKey(k, this.limit);
+  var bucket = this.storage[index];
+  if (bucket) {
+    for (var i = 0; i < bucket.length; i++) {
+      if (k === bucket[i][0]) {
+        bucket.splice(i, 1);
+        if(!bucket.length) {
+          delete this.storage[index];
+        }
+      }
+    }
+  }
 };
